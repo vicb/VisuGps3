8cbf72bc55f51817a0cf3d761abd6d871af1e9a7
Victor <victor@suumit.com>
2013-11-27 09:17:53 +0100

    Update font awesome + CDN

--- src/vgps3/plugins/chart/sliders.js
+++ src/vgps3/plugins/chart/sliders.js
@@ -126,4 +126,4 @@ vgps3.chart.Sliders.prototype.disposeInternal = function() {
 /**
  * @define {string}
  */
-vgps3.chart.Sliders.title_ = '<h1>VisuGps&nbsp;<span class="icon-question-sign"></span></h1>';
+vgps3.chart.Sliders.title_ = '<h1>VisuGps&nbsp;<i class="fa fa-question-circle"></i></h1>';
