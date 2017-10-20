2f8b1b77bfbf9248cd1c4a351a94ecc104f8dee4
Victor Berchet <victor@suumit.com>
2017-10-20 14:02:17 -0700

    Update Spain server

--- src/vgps3/plugins/topo/es.js
+++ src/vgps3/plugins/topo/es.js
@@ -18,9 +18,7 @@ goog.provide('vgps3.topo.es.Map');
 
 goog.require('vgps3.Map');
 goog.require('vgps3.proj.GProj');
-goog.require('vgps3.proj.Iberpix');
 goog.require('vgps3.topo.AbstractTopo');
-
 
 
 /**
@@ -28,30 +26,6 @@ goog.require('vgps3.topo.AbstractTopo');
  * @extends {vgps3.topo.AbstractTopo}
  */
 vgps3.topo.es.Map = function() {
-  /**
-   * @type {google.maps.MapsEventListener} Listen to zoom changes
-   * @private
-   */
-  this.zoomListener_;
-
-  /**
-   * @type {google.maps.MapsEventListener} Listen to map moves
-   * @private
-   */
-  this.moveListener_;
-
-  /**
-   * @type {number} TM zone
-   * @private
-   */
-  this.zone_ = 30;
-
-  /**
-   * @type {vgps3.proj.Iberpix}
-   * @private
-   */
-  this.projection_ = new vgps3.proj.Iberpix(this.zone_);
-
   goog.base(this);
 };
 goog.inherits(vgps3.topo.es.Map, vgps3.topo.AbstractTopo);
@@ -67,86 +41,18 @@ vgps3.topo.es.Map.prototype.init = function(vgps) {
   this.registerMapType_(vgps3.topo.es.MapTypeId.TERRAIN);
 };
 
-
-/**
- * @override
- */
-vgps3.topo.es.Map.prototype.showHandler_ = function() {
-  goog.base(this, 'showHandler_');
-  this.updateProjection_(true);
-
-  this.zoomListener_ = google.maps.event.addListener(
-      this.gMap_,
-      'zoom_changed',
-      goog.bind(this.updateProjection_, this)
-      );
-
-  this.moveListener_ = google.maps.event.addListener(
-      this.gMap_,
-      'center_changed',
-      goog.bind(this.updateProjection_, this)
-      );
-};
-
-
-/**
- * @override
- */
-vgps3.topo.es.Map.prototype.hideHandler_ = function() {
-  goog.base(this, 'hideHandler_');
-  google.maps.event.removeListener(this.zoomListener_);
-  google.maps.event.removeListener(this.moveListener_);
-};
-
-
-/**
- * Updates the projection when the map moves.
- *
- * @param {boolean=} opt_force Whether to force the update.
- *
- * @private
- */
-vgps3.topo.es.Map.prototype.updateProjection_ = function(opt_force) {
-  var previousZone = this.zone_,
-      zoom = this.gMap_.getZoom(),
-      center = this.gMap_.getCenter();
-
-  if (zoom < 11) {
-    this.zone_ = 30;
-  } else {
-    var centerLng = center.lng();
-    if (centerLng < -6) {
-      this.zone_ = 29;
-    } else if (centerLng < 0) {
-      this.zone_ = 30;
-    } else {
-      this.zone_ = 31;
-    }
-  }
-
-  if (opt_force || this.zone_ !== previousZone) {
-    this.projection_.setZone(this.zone_);
-    this.gMap_.setCenter(center);
-  }
-};
-
-
 /**
  * @override
  */
 vgps3.topo.es.Map.prototype.getMapType_ = function() {
-  var mapType = new google.maps.ImageMapType({
+  return new google.maps.ImageMapType({
     getTileUrl: goog.bind(this.getTileUrl_, this),
     tileSize: new google.maps.Size(256, 256),
-    minZoom: vgps3.topo.es.ZOOM_OFFSET,
+    minZoom: 5,
     maxZoom: 18,
     name: 'Espagne',
     alt: 'Cartes IGN Espagne'
   });
-
-  mapType.projection = new vgps3.proj.GProj(this.projection_, 2048 * Math.pow(2, vgps3.topo.es.ZOOM_OFFSET));
-
-  return mapType;
 };
 
 
@@ -159,40 +65,17 @@ vgps3.topo.es.Map.prototype.getTileUrl_ = function(coord, zoom) {
   if (!this.isTileVisible_(coord, zoom)) {
     return null;
   }
+
   return vgps3.topo.es.TILES_URL
-        .replace('{server}', (vgps3.topo.es.serverIndex_++ % 5).toString())
-        .replace('{layer}', this.getLayerName_(zoom))
-        .replace('{zone}', this.zone_.toString())
-        .replace('{scale}', (2048 / Math.pow(2, zoom - vgps3.topo.es.ZOOM_OFFSET) * 1000).toString())
+        .replace('{zoom}', zoom.toString())
         .replace('{x}', (((coord.x % numTiles) + numTiles) % numTiles).toString())
-        .replace('{y}', (-coord.y - 1).toString());
+        .replace('{y}', coord.y.toString());
 };
-
-
-/**
- * Returns the layer name according to the zoom level.
- *
- * @param {number} zoomLevel
- * @return {string}
- */
-vgps3.topo.es.Map.prototype.getLayerName_ = function(zoomLevel) {
-  if (zoomLevel < 11) {
-    return vgps3.topo.es.LAYER_NAMES[0];
-  }
-  if (zoomLevel < 13) {
-    return vgps3.topo.es.LAYER_NAMES[1];
-  }
-  if (zoomLevel < 15) {
-    return vgps3.topo.es.LAYER_NAMES[2];
-  }
-  return vgps3.topo.es.LAYER_NAMES[3];
-};
-
 
 /**
  * @define {string} The url for tiles.
  */
-vgps3.topo.es.TILES_URL = 'http://ts{server}.iberpix.ign.es/tileserver/n={layer};z={zone};r={scale};i={x};j={y}.jpg';
+vgps3.topo.es.TILES_URL = 'https://www.ign.es/wmts/mapa-raster?layer=MTN&style=default&tilematrixset=GoogleMapsCompatible&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image/jpeg&TileMatrix={zoom}&TileCol={x}&TileRow={y}';
 
 
 /**
@@ -201,22 +84,6 @@ vgps3.topo.es.TILES_URL = 'http://ts{server}.iberpix.ign.es/tileserver/n={layer}
 vgps3.topo.es.MapTypeId = {
   TERRAIN: 'vgps3-topo-es-terrain'
 };
-
-
-/**
- * The layer names according to zoom level [<11, <13, <15, >=15]
- * @const
- * @type {Array.<string>}
- */
-vgps3.topo.es.LAYER_NAMES = ['mapa_millon', 'mapa_mtn200', 'mapa_mtn50', 'mapa_mtn25'];
-
-
-/**
- * @const
- * @type {number} 2^ZOOM_OFFSET should be >= number of tiles required to cover the whole world
- */
-vgps3.topo.es.ZOOM_OFFSET = Math.round(Math.log(2 * Math.PI * 6378137 / (2048 * 256)) / Math.LN2);
-
 
 /**
  * @type {number}
